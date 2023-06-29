531b94e48c299d8086a3517bbb82b5ff2296befe
CohorteEich <cohorteeichgenerationc@gmail.com>
2023-06-29 00:42:23 -0400

    ejemplos form html

---
+++ HTML-clases/formulario.js
@@ -0,0 +1,9 @@
+const formularioDOM = document.querySelector("#formm");
+
+/* 
+evita el renderizado de la pagina  */
+/* formularioDOM.addEventListener("submit",function(e) {
+    e.preventDefault();
+console.log("evento ok");
+alert("se envio tu fornulario");
+}); */
